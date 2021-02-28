4a18cdb24e198301c2e12175df594db4d47a23f8
KevenKS <keven.sp@hotmail.com>
2021-02-28 18:03:55 -0300

    Teoria simples sobre excecoes

---
+++ Excecoes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Excecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exceção é um obj herdado das subclasses SystemException ou ApplicationException
+            // SystemException é exceções de sistemas, feita pelo .NET
+
+            //Exception
+            //      SystemException
+            //          IndexOutOfRangeException (Tenta acessar a posição de um array e ela não existe)
+            //          NullReferenceException
+            //          InvalidCastException
+            //          OutOfMemoryException
+            //          ArgumenteException
+            //              ArgumentNullException
+            //              ArgumenteOutOfRangeException
+            //          ExternalException
+            //              ComExcepiton
+            //              SEHException
+            //          ArithmeticException
+            //              DivideByZeroException
+            //              OverflowException (Estourar o limite de um certo numero)
+
+            // ApplicationException = é exceções personalizadas pelo programador
+        }
+    }
+}
